07f85883d9726efec0662d5645de486af9a38c28
Probo Krishnacahya <72754203+cahyaa5758@users.noreply.github.com>
2021-06-06 18:26:39 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['username'])) {
-	header("location: index_akun_admin.php");
+    header("location: index_akun_admin.php");
 }
 ?>
 
@@ -17,11 +17,20 @@ if (!isset($_SESSION['username'])) {
     <link rel="stylesheet" href="table_article.css" />
     <link rel="stylesheet" href="admin_article.css" />
     <link rel="stylesheet" href="index_login_admin.css" />
+    <link rel="icon" href="favicon.png" type="image/png" />
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@600&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
     <title>Read Data</title>
 </head>
 
 <body>
-    <br><br><h1>READ ALL ARTIKEL DATA</h1>
+
+    <button class="logout"><a class="logoutText" href="logout_admin.php">Logout</a></button>
+    <button class="create"><a class="indexText" href="create.php"><b>Create</b></a></button>
+    <br>
+    <h1>CRUD All Article Data</h1>
+
     <table class="readAdmin">
         <tr>
             <td class="td_readAdmin"><b>ID</b></td>
@@ -48,7 +57,7 @@ if (!isset($_SESSION['username'])) {
         }
         ?>
     </table>&emsp;
-    <button class="logout"><a class="logoutText" href="logout_admin.php">Logout</a></button>
+
 </body>
 
 </html>
